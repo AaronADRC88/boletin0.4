f05c18f571dd2b2c85c5e86c1f56a2e6eb1f9305
aferreiradominguez <aferreiradominguez@eniac27.danielcastelao.org>
2014-10-09 10:44:33 +0200

    obxetos

---
+++ src/boletin0/pkg4/Boletin04.java
@@ -0,0 +1,15 @@
+/*obxetos*/
+package boletin0.pkg4;
+
+public class Boletin04 {
+
+    public static void main(String[] args) {
+//instancio obxeto tipo semaforo
+        Semaforo sema1=new Semaforo();
+        String valor=sema1.dameCor();
+       System.out.println("cor antes darlle un valor "+valor);
+       
+       
+    }
+    
+}
